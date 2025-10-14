26b479c41e961a958ea5a1c0f85fdf53595a4669
Ivan <ivanismael.oviedo@elorrieta-errekamari.com>
2025-10-14 21:20:05 +0200

    ruben marizonaza

---
+++ Spinning Cat/src/pojos/Ejercicio.java
@@ -0,0 +1,30 @@
+package pojos;
+
+public class Ejercicio {
+
+	private String nombre;
+	private int duracion;
+
+	public Ejercicio(String nombre, int duracion) {
+		super();
+		this.nombre = nombre;
+		this.duracion = duracion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+}
---
+++ Spinning Cat/src/pojos/Usuario.java
@@ -0,0 +1,6 @@
+package pojos;
+
+public class Usuario {
+
+	
+}
