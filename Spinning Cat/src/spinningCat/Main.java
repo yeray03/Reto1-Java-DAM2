020b6e54ab3155a64b361d9e392e48ed737db1e9
Ivan <ivanismael.oviedoza@elorrieta-errekamari.com>
2025-10-16 02:52:30 +0200

    LoginPanel and WorkoutsPanel added

--- Spinning Cat/src/spinningCat/Main.java
+++ Spinning Cat/src/spinningCat/Main.java
@@ -1,9 +1,13 @@
 package spinningCat;
 
+import vista.LoginPanel;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		javax.swing.SwingUtilities.invokeLater(() -> {
+			new LoginPanel();
+		});
 	}
 
 }
